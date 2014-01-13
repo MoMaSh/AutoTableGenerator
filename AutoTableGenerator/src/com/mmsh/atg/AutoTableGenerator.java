b2dc02b112a013c014c03c332b4e50c2aac5f7d5
mshh <mshh@OSBLAP177.idvnet.local>
2014-01-13 09:19:08 +0100

    Signed-off-by: mshh <mshh@OSBLAP177.idvnet.local>

--- AutoTableGenerator/src/com/mmsh/atg/AutoTableGenerator.java
+++ AutoTableGenerator/src/com/mmsh/atg/AutoTableGenerator.java
@@ -2,18 +2,27 @@ package com.mmsh.atg;
 
 import org.tepi.filtertable.FilterTable;
 
+import com.vaadin.data.Container;
+
 @SuppressWarnings("serial")
 public class AutoTableGenerator extends FilterTable {
 
-	public AutoTableGenerator() {
-	}
-
-	public AutoTableGenerator(String caption) {
-		setCaption(caption);
-	}
-	
 	public AutoTableGenerator(Class<?> clazz) {
 		
 	}
+	
+	public AutoTableGenerator(String caption, Class<?> clazz) {
+		
+	}
+
+	public AutoTableGenerator(Container dataSource) {
+		
+	}
+	
+	public AutoTableGenerator(String caption, Container dataSource) {
+		
+	}
+	
+	
 	
 }
